bf79db8f2a26210aeaeb29e3a8b2414965f6328c
Juan Cruz Viotti <jviottidc@gmail.com>
2016-03-24 09:49:58 -0400

    Cope with raw devices automatically

--- tests/utils.spec.js
+++ tests/utils.spec.js
@@ -9,8 +9,16 @@ describe('Utils', function() {
       chai.expect(utils.getDeviceBSDName('/dev/disk2')).to.equal('disk2');
     });
 
-    it('should return the same thing  given a BSD name', function() {
+    it('should return the same thing given a BSD name', function() {
       chai.expect(utils.getDeviceBSDName('disk2')).to.equal('disk2');
+    });
+
+    it('should return the BSD name given a full path to a raw device', function() {
+      chai.expect(utils.getDeviceBSDName('/dev/rdisk2')).to.equal('disk2');
+    });
+
+    it('should return the BSD name given a raw device name', function() {
+      chai.expect(utils.getDeviceBSDName('rdisk2')).to.equal('disk2');
     });
 
   });
